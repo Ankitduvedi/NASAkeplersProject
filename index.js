801a4a3b7ef6db463e7dfcc5d1a090887cfd8cf0
Ankit kumar duvedi <ankitduvedi.03@gmail.com>
2022-10-27 01:44:31 +0530

    index.js

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const {parse } = require ('csv-parse');
 const fs = require('fs');
 
 const results = [];
+
+function isHabitableplanet (planet){
+    return planet[koi_disposition] === 'CONFIRMED'
+}
 fs.createReadStream('kepler_data.csv')
     .pipe(parse({
-        comment: '#',
+        comment: '#',  // this will tell the function that which line are commented
         columns: true ,
 
     }))
     .on('data', (data)=>{
+        if(isHabitableplanet(data)){
         results.push(data);
+        }
     })
     .on('error', (error)=>{
         console.log(error);
